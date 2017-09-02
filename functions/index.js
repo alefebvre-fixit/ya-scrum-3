8ca802dce7cf1c3a7dd0e9e52b5389de48192fa8
antoine lefebvre <antoine_lefebvre@calypso.com>
2017-09-02 15:48:30 -0700

    cloud function for thumbnail

--- functions/index.js
+++ functions/index.js
@@ -1,66 +1,111 @@
+/**
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for t`he specific language governing permissions and
+ * limitations under the License.
+ */
 'use strict';
 
 const functions = require('firebase-functions');
 const mkdirp = require('mkdirp-promise');
+
+// Include a Service Account Key to use a Signed URL
 const gcs = require('@google-cloud/storage')();
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
 const spawn = require('child-process-promise').spawn;
 const path = require('path');
 const os = require('os');
 const fs = require('fs');
 
-// File extension for the created JPEG files.
-const JPEG_EXTENSION = '.jpg';
+// Max height and width of the thumbnail in pixels.
+const THUMB_MAX_HEIGHT = 200;
+const THUMB_MAX_WIDTH = 200;
+// Thumbnail prefix added to file names.
+const THUMB_PREFIX = 'thumb_';
 
 /**
- * When an image is uploaded in the Storage bucket it is converted to JPEG automatically using
+ * When an image is uploaded in the Storage bucket We generate a thumbnail automatically using
  * ImageMagick.
+ * After the thumbnail has been generated and uploaded to Cloud Storage,
+ * we write the public URL to the Firebase Realtime Database.
  */
-exports.imageToJPG = functions.storage.object().onChange(event => {
-  const object = event.data;
-  const filePath = object.name;
-  const baseFileName = path.basename(filePath, path.extname(filePath));
+exports.generateThumbnail = functions.storage.object().onChange(event => {
+  // File and directory paths.
+  const filePath = event.data.name;
   const fileDir = path.dirname(filePath);
-  const JPEGFilePath = path.normalize(path.format({dir: fileDir, name: baseFileName, ext: JPEG_EXTENSION}));
+  const fileName = path.basename(filePath);
+  const thumbFilePath = path.normalize(path.join(fileDir, `${THUMB_PREFIX}${fileName}`));
   const tempLocalFile = path.join(os.tmpdir(), filePath);
   const tempLocalDir = path.dirname(tempLocalFile);
-  const tempLocalJPEGFile = path.join(os.tmpdir(), JPEGFilePath);
+  const tempLocalThumbFile = path.join(os.tmpdir(), thumbFilePath);
 
   // Exit if this is triggered on a file that is not an image.
-  if (!object.contentType.startsWith('image/')) {
+  if (!event.data.contentType.startsWith('image/')) {
     console.log('This is not an image.');
     return;
   }
 
-  // Exit if the image is already a JPEG.
-  if (object.contentType.startsWith('image/jpeg')) {
-    console.log('Already a JPEG.');
+  // Exit if the image is already a thumbnail.
+  if (fileName.startsWith(THUMB_PREFIX)) {
+    console.log('Already a Thumbnail.');
     return;
   }
 
   // Exit if this is a move or deletion event.
-  if (object.resourceState === 'not_exists') {
+  if (event.data.resourceState === 'not_exists') {
     console.log('This is a deletion event.');
     return;
   }
 
-  const bucket = gcs.bucket(object.bucket);
+  // Cloud Storage files.
+  const bucket = gcs.bucket(event.data.bucket);
+  const file = bucket.file(filePath);
+  const thumbFile = bucket.file(thumbFilePath);
+
   // Create the temp directory where the storage file will be downloaded.
   return mkdirp(tempLocalDir).then(() => {
     // Download file from bucket.
-    return bucket.file(filePath).download({destination: tempLocalFile});
+    return file.download({destination: tempLocalFile});
   }).then(() => {
-    console.log('The file has been downloaded to',
-        tempLocalFile);
-    // Convert the image to JPEG using ImageMagick.
-    return spawn('convert', [tempLocalFile, tempLocalJPEGFile]);
+    console.log('The file has been downloaded to', tempLocalFile);
+    // Generate a thumbnail using ImageMagick.
+    return spawn('convert', [tempLocalFile, '-thumbnail', `${THUMB_MAX_WIDTH}x${THUMB_MAX_HEIGHT}>`, tempLocalThumbFile]);
   }).then(() => {
-    console.log('JPEG image created at', tempLocalJPEGFile);
-    // Uploading the JPEG image.
-    return bucket.upload(tempLocalJPEGFile, {destination: JPEGFilePath});
+    console.log('Thumbnail created at', tempLocalThumbFile);
+    // Uploading the Thumbnail.
+    return bucket.upload(tempLocalThumbFile, {destination: thumbFilePath});
   }).then(() => {
-    console.log('JPEG image uploaded to Storage at', JPEGFilePath);
-    // Once the image has been converted delete the local files to free up disk space.
-    fs.unlinkSync(tempLocalJPEGFile);
+    console.log('Thumbnail uploaded to Storage at', thumbFilePath);
+    // Once the image has been uploaded delete the local files to free up disk space.
     fs.unlinkSync(tempLocalFile);
-  });
+    fs.unlinkSync(tempLocalThumbFile);
+    // Get the Signed URLs for the thumbnail and original image.
+    // const config = {
+    //   action: 'read',
+    //   expires: '03-01-2500'
+    // };
+    // return Promise.all([
+    //   thumbFile.getSignedUrl(config),
+    //   file.getSignedUrl(config)
+    // ]);
+  }).then(results => {
+    // console.log('Got Signed URLs.');
+    // const thumbResult = results[0];
+    // const originalResult = results[1];
+    // const thumbFileUrl = thumbResult[0];
+    // const fileUrl = originalResult[0];
+    // // Add the URLs to the Database
+    // return admin.database().ref('images').push({path: fileUrl, thumbnail: thumbFileUrl});
+  }).then(() => console.log('Thumbnail URLs saved to database.'));
 });
