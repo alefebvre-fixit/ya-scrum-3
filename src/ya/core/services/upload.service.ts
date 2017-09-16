768df2e27a73034f7bf9406efed02a7580d89964
antoine lefebvre <antoine_lefebvre@calypso.com>
2017-09-16 16:39:38 -0700

    update firebase and c3

--- src/ya/core/services/upload.service.ts
+++ src/ya/core/services/upload.service.ts
@@ -34,7 +34,7 @@ export class UploadService {
         uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED,
             (snapshot) => {
                 // upload in progress
-                upload.progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                //upload.progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
             },
             (error) => {
                 // upload failed
@@ -46,51 +46,6 @@ export class UploadService {
                 upload.name = upload.file.name;
             }
         );
-    }
-
-    pushUpload(upload: Upload) {
-
-        // Get a reference to the storage service, which is used to create references in your storage bucket
-        var storage = this.firebaseApp.storage();
-
-        console.log(storage);
-
-        // Create a storage reference from our storage service
-        var storageRef = storage.ref();
-
-        console.log(storageRef);
-
-
-        const uploadTask = storageRef.child(`${this.basePath}/${upload.file.name}`).put(upload.file);
-        uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED,
-            (snapshot) => {
-                // upload in progress
-                upload.progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-            },
-            (error) => {
-                // upload failed
-                console.log(error);
-            },
-            () => {
-                // upload success
-                upload.url = uploadTask.snapshot.downloadURL;
-                upload.name = upload.file.name;
-                this.saveFileData(upload);
-            }
-        );
-    }
-
-    // Writes the file details to the realtime db
-    private saveFileData(upload: Upload) {
-        this.db.list(`${this.basePath}/`).push(upload);
-    }
-
-    deleteUpload(upload: Upload) {
-        this.deleteFileData(upload.$key)
-            .then(() => {
-                this.deleteFileStorage(upload.name)
-            })
-            .catch(error => console.log(error))
     }
 
     // Deletes the file details from the realtime db
